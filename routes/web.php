09e685a6748e18451630f6b26a7ef06b78c83cd1
Mayssam Alwaraa <mayssamalwaraa295@gmail.com>
2024-10-28 21:52:50 +0100

    front theme integration

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('front.index');
+Route::name('front.')->group(function(){
+    Route::view('/','front.index')->name('index');
+    Route::view('/about','front.about')->name('about');
+    Route::view('/service','front.service')->name('service');
+    Route::view('/contact','front.contact')->name('contact');
+
+
+
 });
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
