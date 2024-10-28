a08e6d738b7e1bb37c662eb2d595a6e1b24db3f4
Mayssam Alwaraa <mayssamalwaraa295@gmail.com>
2024-10-28 22:32:36 +0100

    admin theme integration

---
+++ resources/views/admin/master.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+  @include('admin.partials.head')
+  <body class="vertical  light  ">
+    <div class="wrapper">
+      @include('admin.partials.nav')
+      @include('admin.partials.sidebar')
+      <main role="main" class="main-content">
+        @yield('content')
+       
+      </main> <!-- main -->
+    </div> <!-- .wrapper -->
+   @include('admin.partials.scripts')
+  </body>
+</html>
